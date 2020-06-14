37ba9b374507cf9f1f2255e97fb8c273bca2adee
Maxim Ovchinnikov <maksim.v.ovchinnikov@gmail.com>
2020-06-14 14:49:33 +0500

    update gitignore

--- .zsh.sh
+++ .zsh.sh
@@ -11,9 +11,9 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
-echo "------------------------------"
-echo "Installing Homebrew package manager."
-echo "------------------------------"
+  echo "------------------------------"
+  echo "Installing Homebrew package manager"
+  echo "------------------------------"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
@@ -26,13 +26,16 @@ brew install zsh
 # Install Oh My Zsh
 if [[ ! -d ~/.oh-my-zsh ]]
 then
-    echo "------------------------------"
-    echo "Installing Oh My Zsh."
-    echo "------------------------------"
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo "------------------------------"
+  echo "Installing Oh My Zsh"
+  echo "------------------------------"
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 # Install Powerline10k zsh theme
+echo "------------------------------"
+echo "Installing powerlevel10k theme"
+echo "------------------------------"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 brew install romkatv/powerlevel10k/powerlevel10k
 
